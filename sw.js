9d9956ce4eeeeb8dd5b922367fd5abde9dabe4b4
poeggi <48416618+poeggi@users.noreply.github.com>
2026-06-20 21:53:41 +0200

    Perfect level bonus, fireworks, mute+FPS above canvas, ESC mobile btn, dpad smaller, version label

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this string with every deployment to force cache refresh on all clients
-const CACHE = 'snake-v1';
+const CACHE = 'snake-v3';
 const ASSETS = ['./', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
