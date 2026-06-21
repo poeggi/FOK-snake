2c203451b2d649ab9feb544de63ac5cf7250db53
poeggi <48416618+poeggi@users.noreply.github.com>
2026-06-21 23:59:43 +0200

    Fix LIVES label missing trailing space in HUD

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this string with every deployment to force cache refresh on all clients
-const CACHE = 'snake-v45';
+const CACHE = 'snake-v46';
 const ASSETS = ['./', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
