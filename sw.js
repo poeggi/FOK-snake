d9b72d23b5be46c6cf1e92a498980a06546152ec
poeggi <48416618+poeggi@users.noreply.github.com>
2026-06-21 13:31:37 +0200

    Visual direction feedback: eyes and dot shift to queued direction on keypress

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this string with every deployment to force cache refresh on all clients
-const CACHE = 'snake-v26';
+const CACHE = 'snake-v27';
 const ASSETS = ['./', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
