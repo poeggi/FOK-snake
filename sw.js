5f10a750c136e5fa0da8c65f939d2562c75de873
poeggi <48416618+poeggi@users.noreply.github.com>
2026-06-21 23:57:10 +0200

    Fix GEMS label missing trailing space in HUD

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this string with every deployment to force cache refresh on all clients
-const CACHE = 'snake-v44';
+const CACHE = 'snake-v45';
 const ASSETS = ['./', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
