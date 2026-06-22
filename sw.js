c8701442d048bdbfc60bb15b53b2d0e3964a01c4
poeggi <48416618+poeggi@users.noreply.github.com>
2026-06-22 14:15:27 +0200

    Block Escape and Tab from exiting splash (no audio permission)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this string with every deployment to force cache refresh on all clients
-const CACHE = 'snake-v9-2';
+const CACHE = 'snake-v9-3';
 const ASSETS = ['./', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
