c273ffdd4f991da7e29ef5f0d16e35626b84c069
poeggi <48416618+poeggi@users.noreply.github.com>
2026-06-22 07:06:55 +0200

    Credits: Down=fast forward x2, Up=slow down /2 (range 0.1-6.4x)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this string with every deployment to force cache refresh on all clients
-const CACHE = 'snake-v54';
+const CACHE = 'snake-v55';
 const ASSETS = ['./', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
