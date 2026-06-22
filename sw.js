961407929936dcc53ce62aec9677ebaf6326aeb2
poeggi <48416618+poeggi@users.noreply.github.com>
2026-06-22 08:48:29 +0200

    Add levelReady countdown, fix menu music on first interaction, HIGH ROLLER threshold 64k

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this string with every deployment to force cache refresh on all clients
-const CACHE = 'snake-v8-63';
+const CACHE = 'snake-v8-64';
 const ASSETS = ['./', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
