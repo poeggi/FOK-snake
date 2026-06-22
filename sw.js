faf2d645f67f40c348f00c9f4be62f4ea655f788
poeggi <48416618+poeggi@users.noreply.github.com>
2026-06-22 07:22:41 +0200

    Swipe: 40px threshold only on direction changes, same-direction always 30px

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this string with every deployment to force cache refresh on all clients
-const CACHE = 'snake-v57';
+const CACHE = 'snake-v58';
 const ASSETS = ['./', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
