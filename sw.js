d929cf8e858dc414624d0f94347a66c1d6fcf332
poeggi <48416618+poeggi@users.noreply.github.com>
2026-06-22 14:04:01 +0200

    Improve coin animation; fix splash touch bleeding into menu

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this string with every deployment to force cache refresh on all clients
-const CACHE = 'snake-v9-0';
+const CACHE = 'snake-v9-1';
 const ASSETS = ['./', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
