2eb470565a67a12acebaaa8e7c8c068a37650e4d
poeggi <48416618+poeggi@users.noreply.github.com>
2026-06-22 16:43:12 +0200

    Fix accidental swipe boost: only arm boost on same-direction repeat swipe, not on turns

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this string with every deployment to force cache refresh on all clients
-const CACHE = 'snake-v9-14';
+const CACHE = 'snake-v9-15';
 const ASSETS = ['./', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
