c78915ad0060575767c15ea3dbf66240c3a34c56
poeggi <48416618+poeggi@users.noreply.github.com>
2026-06-25 15:53:29 +0200

    Portrait gamepad: PAUSE|ESC top row, OK|START bottom row

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Bump this string with every deployment to force cache refresh on all clients
-// version snake-v1.0.3, released 2026-06-25 15:52 +0200
-const CACHE = 'snake-v1.0.3';
+// version snake-v1.0.4, released 2026-06-25 15:53 +0200
+const CACHE = 'snake-v1.0.4';
 const ASSETS = ['./', './assets.js', './audio.js', './fonts/PressStart2P-Regular.woff2', './game.js', './icon.svg', './manifest.json', './style.css'];
 
 self.addEventListener('install', e => {
